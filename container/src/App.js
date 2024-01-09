dcabb068c0a07c2bf477ec681d5c735da65c84a3
AminGangsarPangarso <amindailylife1976@gmail.com>
2024-01-09 21:50:13 +0700

    shared dependencies antar package json dan build container

--- container/src/App.js
+++ container/src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
-// import {mount} from 'marketing/MarketingApp'
+import MarketingApp from "./components/MarketingApp"
 
 
 export default () =>{
     return <div>
         <h1>INI CONTAINER !</h1>
+        <hr/>
+        <MarketingApp/>
     </div>
 }
